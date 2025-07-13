6593e66c6d6f2750205ce146e76131c9de23ae6c
Alexander Kiefer <Alexander.Kiefer77@protonmail.com>
2025-07-13 16:46:01 +0200

    addes html

---
+++ scripts/db.js
@@ -0,0 +1,53 @@
+
+let myDishes = [
+    {
+        "name": "Pizza Magherita",
+        "price": 6.50,
+        "description": "mit Mozarella"
+    },
+    {
+        "name": "Pizza Alaska",
+        "price": 11.50,
+        "description": "mit Lachs, Spinat und Mozarella"
+    },
+    {
+        "name": "Pizza Krabben",
+        "price": 9.50,
+        "description": "mit Krabben und Peperroni"
+    },
+    {
+        "name": "Pizza Diavolo (scharf)",
+        "price": 9.50,
+        "description": "mit Salami, Zwiebeln, Peperoni und Knoblauch"
+    },
+    {
+        "name": "Pizza Bolognese",
+        "price": 10.50,
+        "description": "mit Putenschinken, Champignons, Hackfleisch und Parmesan"
+    },
+    {
+        "name": "Pizza Salami",
+        "price": 9.50,
+        "description": "mit Rindersalami"
+    },
+    {
+        "name": "Pizza Funghi",
+        "price": 9.50,
+        "description": "mit Champignons und Tomaten"
+    },
+    {
+        "name": "Pizza Prosciutto e Funghi",
+        "price": 10.50,
+        "description": "mit Putenschinken und Champignons"
+    },
+    {
+        "name": "Pizza Speziale",
+        "price": 12.50,
+        "description": "mit Kochschinken, Salami, Champignons, Zwiebeln und Knoblauch"
+    },
+    {
+        "name": "Pizzabrötchen",
+        "price": 12.50,
+        "description": "mit Käse und Knoblauch"
+    }
+]
