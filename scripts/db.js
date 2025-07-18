4821e14384357eafc6b8211f0e26d4f8ae988460
Alexander Kiefer <Alexander.Kiefer77@protonmail.com>
2025-07-18 14:01:06 +0200

    modified db.js

--- scripts/db.js
+++ scripts/db.js
@@ -3,52 +3,72 @@ let myDishes = [
     {
         "name": "Pizza Magherita",
         "price": 6.50,
-        "description": "mit Mozarella"
+        "description": "mit Mozarella",
+        "startnumber": 1,
+        "newprice": 6.50
     },
     {
         "name": "Pizza Alaska",
         "price": 11.50,
-        "description": "mit Lachs, Spinat und Mozarella"
+        "description": "mit Lachs, Spinat und Mozarella",
+        "startnumber": 1,
+        "newprice": 11.50
     },
     {
         "name": "Pizza Krabben",
         "price": 9.50,
-        "description": "mit Krabben und Peperroni"
+        "description": "mit Krabben und Peperroni",
+        "startnumber": 1,
+        "newprice": 9.50
     },
     {
         "name": "Pizza Diavolo (scharf)",
         "price": 9.50,
-        "description": "mit Salami, Zwiebeln, Peperoni und Knoblauch"
+        "description": "mit Salami, Zwiebeln, Peperoni und Knoblauch",
+        "startnumber": 1,
+        "newprice": 9.50
     },
     {
         "name": "Pizza Bolognese",
         "price": 10.50,
-        "description": "mit Putenschinken, Champignons, Hackfleisch und Parmesan"
+        "description": "mit Putenschinken, Champignons, Hackfleisch und Parmesan",
+        "startnumber": 1,
+        "newprice": 10.50
     },
     {
         "name": "Pizza Salami",
         "price": 9.50,
-        "description": "mit Rindersalami"
+        "description": "mit Rindersalami",
+        "startnumber": 1,
+        "newprice": 9.50
     },
     {
         "name": "Pizza Funghi",
         "price": 9.50,
-        "description": "mit Champignons und Tomaten"
+        "description": "mit Champignons und Tomaten",
+        "startnumber": 1,
+        "newprice": 9.50
     },
     {
         "name": "Pizza Prosciutto e Funghi",
         "price": 10.50,
-        "description": "mit Putenschinken und Champignons"
+        "description": "mit Putenschinken und Champignons",
+        "startnumber": 1,
+        "newprice": 10.50
     },
     {
         "name": "Pizza Speziale",
         "price": 12.50,
-        "description": "mit Kochschinken, Salami, Champignons, Zwiebeln und Knoblauch"
+        "description": "mit Kochschinken, Salami, Champignons, Zwiebeln und Knoblauch",
+        "startnumber": 1,
+        "newprice": 12.50
     },
     {
         "name": "Pizzabrötchen",
         "price": 12.50,
-        "description": "mit Käse und Knoblauch"
+        "description": "mit Käse und Knoblauch",
+        "startnumber": 1,
+        "newprice": 12.50
     }
 ]
 
@@ -56,22 +76,30 @@ let mySupplements = [
     {
         "name": "grüner Salat",
         "price": 4.50,
-        "description": "mit Italien Dressing"
+        "description": "mit Italien Dressing",
+        "startnumber": 1,
+        "newprice": 4.50
     },
     {
         "name": "gemischter Salat",
         "price": 4.50,
-        "description": "mit French Dressing"
+        "description": "mit French Dressing",
+        "startnumber": 1,
+        "newprice": 4.50
     },
     {
         "name": "Caprese Salat",
         "price": 6.50,
-        "description": "Tomaten-Büffel Mozzarella Salat"
+        "description": "Tomaten-Büffel Mozzarella Salat",
+        "startnumber": 1,
+        "newprice": 6.50
     },
     {
         "name": "Bruschetta",
         "price": 5.50,
-        "description": "3 Stück, knusprige geröstete Hausbrotscheiben mit gewürfelten Tomaten"
+        "description": "3 Stück, knusprige geröstete Hausbrotscheiben mit gewürfelten Tomaten",
+        "startnumber": 1,
+        "newprice": 5.50
     }
 ]
 
@@ -79,31 +107,43 @@ let myDrinks = [
       {
         "name": "Ur-Pils",
         "price": 3.00,
-        "description": "Das beste Bier, das es gibt, 0.5 l."
+        "description": "Das beste Bier, das es gibt, 0.5 l.",
+        "startnumber": 1,
+        "newprice": 3.00
     },
     {
         "name": "Cola",
         "price": 2.00,
-        "description": "coffein- und kohlensäurehaltiges Erfrischungsgetränk, 0.33 l."
+        "description": "coffein- und kohlensäurehaltiges Erfrischungsgetränk, 0.33 l.",
+        "startnumber": 1,
+        "newprice": 2.00
     },
     {
         "name": "Cola Zero",
         "price": 2.00,
-        "description": "kohlensäurehaltiges Erfrischungsgetränk, 0.33 l."
+        "description": "kohlensäurehaltiges Erfrischungsgetränk, 0.33 l.",
+        "startnumber": 1,
+        "newprice": 2.00
     },
     {
         "name": "Limo",
         "price": 2.00,
-        "description": "Erfrischungsgetränk, 0.33 l."
+        "description": "Erfrischungsgetränk, 0.33 l.",
+        "startnumber": 1,
+        "newprice": 2.00
     },
     {
         "name": "Mineralwasser",
         "price": 2.00,
-        "description": "still, 0.33 l."
+        "description": "still, 0.33 l.",
+        "startnumber": 1,
+        "newprice": 2.00
     },
     {
         "name": "Mineralwasser",
         "price": 2.00,
-        "description": "mit Kohlensäure, 0.33 l."
+        "description": "mit Kohlensäure, 0.33 l.",
+        "startnumber": 1,
+        "newprice": 2.00
     }
 ]
